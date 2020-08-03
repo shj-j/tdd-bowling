48d81f882003ba8f82b971eee9d41881e5ecdd7c
shj-j <shang.jie@outlook.com>
2020-08-04 07:46:50 +0800

    finished test

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -17,5 +17,37 @@ public class BowlingGameTest {
         assertEquals(20, bowlingGame.getScore(numbers));
     }
 
+    @Test
+    public void should_return_102_if_strike(){
+        Integer[] numbers = {4,3,7,1,10,0,10,0,4,0,3,4,3,4,8,1,6,0,1,0};
+        assertEquals(102, bowlingGame.getScore(numbers));
+    }
+
+
+    @Test
+    public void should_return_131_if_strike_in_last_frame_and_first_nine_frame(){
+        Integer[] numbers = {4,3,7,1,10,0,10,0,4,0,3,4,3,4,8,1,6,0,10,10,10};
+        assertEquals(131, bowlingGame.getScore(numbers));
+    }
+
+    @Test
+    public void should_return_74_if_spare(){
+        Integer[] numbers = {1,9,7,1,4,3,7,1,4,0,3,4,3,4,8,1,6,0,1,0};
+        assertEquals(74, bowlingGame.getScore(numbers));
+    }
+
+    @Test
+    public void should_return_84_if_spare_in_last_frame_and_first_nine_frame(){
+        Integer[] numbers = {1,9,7,1,4,3,7,1,4,0,3,4,3,4,8,1,6,0,9,1,1};
+        assertEquals(84, bowlingGame.getScore(numbers));
+    }
+
+    @Test
+    public void should_return_300_if_all_strike(){
+        Integer[] numbers = {10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,0,10,10,10};
+        assertEquals(300, bowlingGame.getScore(numbers));
+    }
+
+
 
 }
